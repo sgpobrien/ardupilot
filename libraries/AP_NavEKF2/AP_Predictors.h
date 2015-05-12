e5fcea406586afb8a05fc940a5780c0ea5d5ddce
Sean O'Brien <sgpobrien@gmail.com>
2015-05-12 15:08:55 +1000

    AP_NavEKF2: Added store and getPrediction functions.

--- libraries/AP_NavEKF2/AP_Predictors.h
+++ libraries/AP_NavEKF2/AP_Predictors.h
@@ -4,10 +4,8 @@
 #include <AP_Math.h>
 #include <AP_Param.h>
 #include <vectorN.h>
-//#include <matrix3.h>
-//#include <quaternion.h>
 
-#define BUFFER_SIZE  200   // sean buffer size for sensors
+#define BUFFER_SIZE  200    //buffer size for sensors
 #define MAX_MSDELAY  2000   // maximum allowed delay
 
 class AP_Predictors
@@ -15,7 +13,6 @@ class AP_Predictors
 public:
     AP_Predictors();
     typedef float ftype;
-    void BestIndex(AP_Int16 _msecPosDelay);
     void AttitudeModel(Vector3f tilde_q);
     void VelocityModel(Vector3f tilde_Vel);
     void VelocityModel2(Vector3f corrected_tilde_Vel12);
@@ -24,11 +21,17 @@ public:
     void AttitudePredictor(Quaternion quat);
     void PositionPredictor(Vector3f position);
     void PositionPredictor2(Vector3f position);
-    //void PositionPredictor3(Vector3f p_pred, Vector3f p, VectorN<Vector3f,BUFFER_SIZE> stored_p, ftype dtIMU, Vector3f position);
     void VelocityPredictor(Vector3f velocity);
     void VelocityPredictor2(Quaternion quat, Vector3f velocity, AP_Int16 _msecPosDelay);
     void CascadedPredictor(Vector3f tilde_q, Vector3f tilde_Vel, Vector3f corrected_tilde_Vel12, Quaternion quat, ftype dtIMU, AP_Int16 _msecPosDelay, Vector3f velocity, Vector3f position);
-    void BestIndex2(uint32_t *closestTime, uint32_t *closestStoreIndex, uint32_t *timeStamp[BUFFER_SIZE], AP_Int16 _msecPosDelay);
+    void BestIndex(uint32_t &closestTime, uint16_t &closestStoreIndex, uint32_t (&timeStamp)[BUFFER_SIZE], AP_Int16 &_msecPosDelay);
+    void storeDataVector(Vector3f &data, VectorN<Vector3f,BUFFER_SIZE> &buffer, uint32_t &lastStoreTime, uint32_t (&timeStamp)[BUFFER_SIZE], uint16_t &storeIndex);
+    void storeDataQuaternion(Quaternion &data, VectorN<Quaternion,BUFFER_SIZE> &buffer, uint32_t &lastStoreTime, uint32_t (&timeStamp)[BUFFER_SIZE], uint16_t &storeIndex);
+    void getAttitudePrediction(Quaternion &att);
+    void getPositionPrediction(Vector3f &pos);
+    void getPosition2Prediction(Vector3f &pos);
+    void getVelocityPrediction(Vector3f &vel);
+    void getVelocity2Prediction(Vector3f &vel);
 
 private:
     uint32_t imuSampleTime_ms;
@@ -37,9 +40,8 @@ private:
     Quaternion D_q;
     Quaternion D_q_k1;
     float n_D_q_k1;
-    Quaternion q_hat;   //sean prediction of the current quaternion
+    Quaternion q_hat;    // prediction of the current quaternion
     Quaternion q_hat_T_k1;
-    // Vector3f tilde_q;
     float n_tilde_q;
     Quaternion delta_q;
     Matrix3f R_hat_T;
@@ -53,58 +55,80 @@ private:
     Vector3f p_hat_m; // prediction of current position mixed-invariant
     Vector3f d_v_m;  // mixed-invariant
     Quaternion D_Delay;
-    uint16_t bestStoreIndex;
 
-    uint16_t storeIndexIMU;						// arash
-    uint32_t lastAngRateStoreTime_ms;		 // sean
-    VectorN<Vector3f,BUFFER_SIZE> storedAngRate;       //  sean
-    VectorN<Vector3f,BUFFER_SIZE> storeddVelIMU1; //sean
-    VectorN<Vector3f,BUFFER_SIZE>  storeddVelIMU2; //sean
-    uint32_t angRateTimeStamp[BUFFER_SIZE];    		  // sean
+    uint16_t storeIndexIMU;
+    uint32_t lastAngRateStoreTime_ms;
+    VectorN<Vector3f,BUFFER_SIZE> storedAngRate;
+    VectorN<Vector3f,BUFFER_SIZE> storeddVelIMU1;
+    VectorN<Vector3f,BUFFER_SIZE>  storeddVelIMU2;
+    uint32_t angRateTimeStamp[BUFFER_SIZE];
 
     uint16_t storeIndexMag;
     uint32_t lastMagStoreTime_ms;
-    VectorN<Vector3f,BUFFER_SIZE> storedMag;       //  sean
-    uint32_t MagTimeStamp[BUFFER_SIZE];    		  // sean
+    VectorN<Vector3f,BUFFER_SIZE> storedMag;
+    uint32_t MagTimeStamp[BUFFER_SIZE];
 
+    uint16_t storeIndexTas;
+    uint32_t lastTasStoreTime_ms;
+    float storedTas[BUFFER_SIZE];
+    uint32_t TasTimeStamp[BUFFER_SIZE];
 
-    uint16_t storeIndexTas;              // sean
-    uint32_t lastTasStoreTime_ms;           // sean
-    float storedTas[BUFFER_SIZE];                 // sean
-    uint32_t TasTimeStamp[BUFFER_SIZE];    		  // sean
+    uint16_t storeIndexHgt;
+    uint32_t lastHgtStoreTime_ms;
+    float storedHgt[BUFFER_SIZE];
+    uint32_t HgtTimeStamp[BUFFER_SIZE];
+    uint32_t lastHealthyHgtTime_ms;  //time the barometer was last declared healthy
 
-    uint16_t storeIndexHgt;              // sean
-    uint32_t lastHgtStoreTime_ms;           // sean
-    float storedHgt[BUFFER_SIZE];                 // sean
-    uint32_t HgtTimeStamp[BUFFER_SIZE];    		  // sean
-    uint32_t lastHealthyHgtTime_ms; // Sean time the barometer was last declared healthy
+    // attitude model variables
+    uint16_t storeIndexD;
+    uint32_t lastDStoreTime_ms;
+    uint32_t DTimeStamp[BUFFER_SIZE];
+    VectorN<Quaternion,BUFFER_SIZE> storedD;
+    uint32_t bestTimeD;
+    uint16_t bestStoreIndexD;
 
-    uint16_t storeIndexD;						// sean
-    uint32_t lastDStoreTime_ms;		 // sean
-    VectorN<Vector3f,BUFFER_SIZE> storedD_v;       //  sean vector part of quaternion Delta
-    float storedD_s[BUFFER_SIZE];                 // sean  scalar part of quaternion Delta
-    uint32_t DTimeStamp[BUFFER_SIZE];    		  // sean
-
-
-    uint16_t storeIndexd_v;						// sean
+    // velocity model variables
+    uint16_t storeIndexd_v;
     uint32_t lastd_vStoreTime_ms;
     uint32_t d_vTimeStamp[BUFFER_SIZE];
-    VectorN<Vector3f,BUFFER_SIZE> storedd_v;       //  sean buffer for delta corrsponding to velocity prediction
-    VectorN<Vector3f,BUFFER_SIZE> storedd_p;       //  sean buffer for delta corrsponding to position prediction
-    VectorN<Vector3f,BUFFER_SIZE> storedd_p_m;       //  sean buffer for delta corrsponding to position prediction mixed-invariant
-    VectorN<Vector3f,BUFFER_SIZE> storedd_v_m;       //  sean buffer for delta corrsponding to velocity prediction mixed-invariant
+    VectorN<Vector3f,BUFFER_SIZE> storedd_v;        //buffer for delta corrsponding to velocity prediction
+    uint32_t bestTimed_v;
+    uint16_t bestStoreIndexd_v;
+
+    // position model variables
+    uint16_t storeIndexd_p;
+    uint32_t lastd_pStoreTime_ms;
+    uint32_t d_pTimeStamp[BUFFER_SIZE];
+    VectorN<Vector3f,BUFFER_SIZE> storedd_p;        //buffer for delta corrsponding to position prediction
+    uint32_t bestTimed_p;
+    uint16_t bestStoreIndexd_p;
+
+    // position model 2 variables
+    uint16_t storeIndexd_p_m;
+    uint32_t lastd_p_mStoreTime_ms;
+    uint32_t d_p_mTimeStamp[BUFFER_SIZE];
+    VectorN<Vector3f,BUFFER_SIZE> storedd_p_m;        //buffer for delta corrsponding to position prediction mixed-invariant
+    uint32_t bestTimed_p_m;
+    uint16_t bestStoreIndexd_p_m;
+
+    // velocity model 2 variables
+    uint16_t storeIndexd_v_m;
+    uint32_t lastd_v_mStoreTime_ms;
+    uint32_t d_v_mTimeStamp[BUFFER_SIZE];
+    VectorN<Vector3f,BUFFER_SIZE> storedd_v_m;        //buffer for delta corrsponding to velocity prediction mixed-invariant
+    uint32_t bestTimed_v_m;
+    uint16_t bestStoreIndexd_v_m;
+
     uint32_t ctr_rst;  // reset predictor cntr
 
-    float init_reset;   //sean reset initial quaternions
+    float init_reset;   // reset initial quaternions
 
     float rotScaler;
     Vector3f D_q_tmp;
     Quaternion q_tmp;
 
-//    Vector3f tilde_Vel;
-//    Vector3f corrected_tilde_Vel1;
-//    Vector3f corrected_tilde_Vel2;
-//    Vector3f corrected_tilde_Vel12;
+    uint32_t bestTime;
+    uint16_t bestStoreIndex;
 
     Matrix3f prevTnb_pred;
     Matrix3f Tbn_temp;
